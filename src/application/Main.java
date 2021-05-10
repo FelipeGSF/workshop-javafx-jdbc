c3066997461aa45b9c2b7bd162396c7d2a8ef849
FelipeGSF <felipegasques@hotmail.com>
2021-05-10 16:06:00 -0400

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,5 @@
 package application;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -12,12 +7,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
-/**
- *
- * @author felip
- */
 public class Main extends Application {
-
+    
+    private static Scene mainScene;
+    
     @Override
     public void start(Stage primaryStage) {
         try {
@@ -27,7 +20,7 @@ public class Main extends Application {
             scrollPane.setFitToHeight(true);
             scrollPane.setFitToWidth(true);
             
-            Scene mainScene = new Scene(scrollPane);
+            mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFX application");
             primaryStage.show();
@@ -36,9 +29,10 @@ public class Main extends Application {
         }
     }
 
-    /**
-     * @param args the command line arguments
-     */
+    public static Scene getMainScene(){
+        return mainScene;
+    }
+    
     public static void main(String[] args) {
         launch(args);
     }
